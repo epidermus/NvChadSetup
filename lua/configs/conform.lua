35b94627bd525c0df80a5e26868edf80e83321a3
epidermus <thelymanboy@gmail.com>
2024-11-06 12:40:46 -0700

    Base Svelte(kit) Themeing Updated

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettierd" },
+    js = { "prettierd" },
+    html = { "prettierd" },
+    svelte = { "prettierd" }
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1000,
+    lsp_fallback = true,
+  },
 }
 
 return options
